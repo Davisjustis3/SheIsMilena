2ed9f9bf54471931b234e3a4f09d27c14d80b07d
Justis <davisjustis3@gmail.com>
2025-01-27 21:21:18 +0100

    settng up for Render deployment

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(cors());
 
 // Create a connection to the database
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Dune2overStarWars',
-    database: 'poems'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 // Connect to the database
